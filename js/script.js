2f81dd31765921544e054ec496f08e5263525523
94Anton94 <54843604+94Anton94@users.noreply.github.com>
2019-11-05 02:41:33 +0500

    поправка перед 2й отправкой

--- js/script.js
+++ js/script.js
@@ -2,5 +2,6 @@ const button = document.querySelector(".button-form-open");
 const form = document.querySelector(".form");
 
 button.addEventListener("click", function (evt) {
+  evt.preventDefault();
   form.classList.toggle("active-form");
 });
