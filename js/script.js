4d676e101fba8b72183995757cc26b79a9e11bfc
94Anton94 <54843604+94Anton94@users.noreply.github.com>
2019-11-06 22:47:26 +0500

    22,48

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 const button = document.querySelector(".button-form-open");
-const form = document.querySelector(".form");
+const forma = document.querySelector(".form");
+
+const in_date = forma.querySelector("[id=check-in-date]");
+const out_date = forma.querySelector("[id=check-out-date]");
+const form = forma.querySelector("form");
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
-  form.classList.toggle("active-form");
+  forma.classList.toggle("active-form");
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!in_date.value || !out_date.value) {
+    evt.preventDefault();
+    forma.classList.add("modal-error");
+    console.log("Нужно ввести даты");
+  }
 });
